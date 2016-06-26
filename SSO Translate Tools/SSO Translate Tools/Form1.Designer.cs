c1e6f9165916e038058bbf495d800d521af17d6f
raffa50 <reloader90@gmail.com>
2016-06-26 12:00:50 +0200

    sorgenti translate tools

---
+++ SSO Translate Tools/SSO Translate Tools/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SSO_Translate_Tools {
+   partial class Form1 {
+      /// <summary>
+      /// Variabile di progettazione necessaria.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Pulire le risorse in uso.
+      /// </summary>
+      /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+      protected override void Dispose( bool disposing ) {
+         if( disposing && ( components != null ) ) {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Codice generato da Progettazione Windows Form
+
+      /// <summary>
+      /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+      /// il contenuto del metodo con l'editor di codice.
+      /// </summary>
+      private void InitializeComponent() {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+         this.BtnDecompile = new System.Windows.Forms.Button();
+         this.btnMerge = new System.Windows.Forms.Button();
+         this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+         this.label1 = new System.Windows.Forms.Label();
+         this.BtnCompress = new System.Windows.Forms.Button();
+         this.BtnLuaRec = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // BtnDecompile
+         // 
+         this.BtnDecompile.Location = new System.Drawing.Point(12, 12);
+         this.BtnDecompile.Name = "BtnDecompile";
+         this.BtnDecompile.Size = new System.Drawing.Size(190, 36);
+         this.BtnDecompile.TabIndex = 0;
+         this.BtnDecompile.Text = "LUA DeCompile";
+         this.BtnDecompile.UseVisualStyleBackColor = true;
+         this.BtnDecompile.Click += new System.EventHandler(this.BtnDecompile_Click);
+         // 
+         // btnMerge
+         // 
+         this.btnMerge.Location = new System.Drawing.Point(12, 96);
+         this.btnMerge.Name = "btnMerge";
+         this.btnMerge.Size = new System.Drawing.Size(190, 36);
+         this.btnMerge.TabIndex = 1;
+         this.btnMerge.Text = "Merge LUA";
+         this.btnMerge.UseVisualStyleBackColor = true;
+         this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+         // 
+         // linkLabel1
+         // 
+         this.linkLabel1.AutoSize = true;
+         this.linkLabel1.Location = new System.Drawing.Point(147, 210);
+         this.linkLabel1.MinimumSize = new System.Drawing.Size(130, 50);
+         this.linkLabel1.Name = "linkLabel1";
+         this.linkLabel1.Size = new System.Drawing.Size(130, 50);
+         this.linkLabel1.TabIndex = 2;
+         this.linkLabel1.TabStop = true;
+         this.linkLabel1.Text = "aldrigo.sf.net";
+         this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(12, 210);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(129, 17);
+         this.label1.TabIndex = 3;
+         this.label1.Text = "By Aldrigo Raffaele";
+         // 
+         // BtnCompress
+         // 
+         this.BtnCompress.Enabled = false;
+         this.BtnCompress.Location = new System.Drawing.Point(12, 138);
+         this.BtnCompress.Name = "BtnCompress";
+         this.BtnCompress.Size = new System.Drawing.Size(190, 36);
+         this.BtnCompress.TabIndex = 4;
+         this.BtnCompress.Text = "Make PCK";
+         this.BtnCompress.UseVisualStyleBackColor = true;
+         // 
+         // BtnLuaRec
+         // 
+         this.BtnLuaRec.Enabled = false;
+         this.BtnLuaRec.Location = new System.Drawing.Point(12, 54);
+         this.BtnLuaRec.Name = "BtnLuaRec";
+         this.BtnLuaRec.Size = new System.Drawing.Size(190, 36);
+         this.BtnLuaRec.TabIndex = 5;
+         this.BtnLuaRec.Text = "LUA ReCompile";
+         this.BtnLuaRec.UseVisualStyleBackColor = true;
+         this.BtnLuaRec.Click += new System.EventHandler(this.BtnLuaRec_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(339, 253);
+         this.Controls.Add(this.BtnLuaRec);
+         this.Controls.Add(this.BtnCompress);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.linkLabel1);
+         this.Controls.Add(this.btnMerge);
+         this.Controls.Add(this.BtnDecompile);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.MaximizeBox = false;
+         this.MinimizeBox = false;
+         this.Name = "Form1";
+         this.Text = "SSO Tranlate Tools";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button BtnDecompile;
+      private System.Windows.Forms.Button btnMerge;
+      private System.Windows.Forms.LinkLabel linkLabel1;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Button BtnCompress;
+      private System.Windows.Forms.Button BtnLuaRec;
+   }
+}
+
---
+++ SSO Translate Tools/SSO Translate Tools/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace SSO_Translate_Tools {
+   public partial class Form1 : Form {
+      private static string _luaDecPath= Application.StartupPath + Path.DirectorySeparatorChar;
+      private readonly CommonOpenFileDialog _folderBrowseDlg= 
+         new CommonOpenFileDialog() { IsFolderPicker= true, InitialDirectory= Application.StartupPath };
+
+      public Form1() {
+         InitializeComponent();
+      }
+
+      public static void Shell( string command ) {
+         var cmd = new Process {
+            StartInfo = {
+               FileName = "cmd.exe",
+               RedirectStandardInput = true,
+               RedirectStandardOutput = true,
+               CreateNoWindow = true,
+               UseShellExecute = false
+            }
+         };
+         cmd.Start();
+
+         cmd.StandardInput.WriteLine( command );
+         cmd.StandardInput.Flush();
+         cmd.StandardInput.Close();
+         cmd.WaitForExit();
+      }
+
+      private static void HandleDir( DirectoryInfo dir, DirectoryInfo dest ) {
+         dest.Create();
+
+         Parallel.ForEach( dir.GetDirectories(), subdir => {
+            var sdd = new DirectoryInfo( dest.FullName + Path.DirectorySeparatorChar + subdir.Name );
+            HandleDir( subdir, sdd );
+         } );
+
+         var luaDecExe = '"' + _luaDecPath + "luadec.exe\"";
+
+         Parallel.ForEach( dir.GetFiles(), fl => {
+            if( fl.Extension == ".lua" ) {
+               var args = luaDecExe + " \"" + fl.FullName + "\" > \"" + dest.FullName + Path.DirectorySeparatorChar + fl.Name + '"';
+               Shell( args );
+            } else 
+               File.Copy( fl.FullName, dest.FullName + Path.DirectorySeparatorChar + fl.Name );
+         } );
+      }
+
+      private void BtnDecompile_Click( object sender, EventArgs e ) {
+         _folderBrowseDlg.Title= "Select folder to decompile";
+         if( _folderBrowseDlg.ShowDialog() != CommonFileDialogResult.Ok )
+            return;
+
+         var source= new DirectoryInfo( _folderBrowseDlg.FileName );
+
+         var dest = new DirectoryInfo( source.FullName + " decompiled" );
+
+         HandleDir( source, dest );
+         MessageBox.Show( "Complete!", "Operation complete", MessageBoxButtons.OK, MessageBoxIcon.Information );
+      }
+
+      private void Form1_Load( object sender, EventArgs e ) {
+         bool quit = false;
+         while( !File.Exists( _luaDecPath + "luadec.exe" ) ) {
+            _folderBrowseDlg.Title= "Select luadec folder";
+            if( _folderBrowseDlg.ShowDialog() == CommonFileDialogResult.Ok ) {
+               _luaDecPath = _folderBrowseDlg.FileName + Path.DirectorySeparatorChar;
+               quit = false;
+            } else
+               quit= true;
+         }
+
+         if( quit )
+            Application.Exit();
+      }
+
+      private void linkLabel1_LinkClicked( object sender, LinkLabelLinkClickedEventArgs e ) {
+         Process.Start( "http://aldrigo.sf.net" );
+      }
+
+      private void Merge( string oldfl, string newfl ) {
+         var oldFile= File.ReadLines( oldfl ).Where( l => l.Length > 0 ).ToList(); //evitare righe vuote
+         var newFile= File.ReadLines( newfl ).Where( l => l.Length > 0 ).ToList();
+
+         if( oldFile.Count() < newFile.Count() ) { //il vecchio file ha meno righe del nuovo
+            File.WriteAllLines( newfl, oldFile.Concat( newFile.Skip( oldFile.Count() ) ) );
+         }
+      }
+
+      private void btnMerge_Click( object sender, EventArgs e ) {
+         _folderBrowseDlg.IsFolderPicker = false;
+         _folderBrowseDlg.Title = "Select old LUA";
+
+         string oldfl, newfl;
+         if( _folderBrowseDlg.ShowDialog() != CommonFileDialogResult.Ok ) {
+            _folderBrowseDlg.IsFolderPicker = true;
+            return;
+         }
+         oldfl = _folderBrowseDlg.FileName;
+
+         _folderBrowseDlg.Title = "Select new LUA";
+         if( _folderBrowseDlg.ShowDialog() != CommonFileDialogResult.Ok ) {
+            _folderBrowseDlg.IsFolderPicker = true;
+            return;
+         }
+         newfl = _folderBrowseDlg.FileName;
+
+         Merge( oldfl, newfl );
+         MessageBox.Show( "Files merged!", "Finished", MessageBoxButtons.OK, MessageBoxIcon.Information );
+      }
+
+      private void BtnLuaRec_Click( object sender, EventArgs e ) {
+         _folderBrowseDlg.Title = "Select folder to compile";
+         if( _folderBrowseDlg.ShowDialog() != CommonFileDialogResult.Ok )
+            return;
+
+         var source = new DirectoryInfo( _folderBrowseDlg.FileName );
+
+         var dest = new DirectoryInfo( source.FullName + " compiled" );
+
+         //HandleDir( source, dest, true );
+         MessageBox.Show( "Complete!", "Operation complete", MessageBoxButtons.OK, MessageBoxIcon.Information );
+      }
+   }
+}
